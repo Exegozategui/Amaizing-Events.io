d9279fd8093028193e1648fec928692f8048f7e2
Exe <exegozategui@gmail.com>
2023-02-27 11:28:28 -0300

    se agrega animacion y estilos

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,7 +5,7 @@ let allCards = newCards(data.events)
 containerCards.innerHTML = allCards
 
 
-
+// Funcion que retorna las card despues de recoorer el array//
 
 
 function newCards(arrayDatos){
@@ -13,13 +13,15 @@ function newCards(arrayDatos){
   for (const event of arrayDatos ) {
     
     cards += `<div class="card-group">
-    <div class="card text-center " style= "width: 18rem;" >
+    <div class="card  text-center border-danger mb-3 " style= "width: 18rem;" >
     <img src=${event.image} class="card-img-top" " alt="...">
     <div class="card-body">
       <h5 class="card-title">${event.name}</h5>
       <p class="card-text">${event.description}</p>
-      <a class="card-text">${event.price}</a>
-      <a href="./Details.html" class="btn btn-primary">ver mas</a>
+      <div class="card-footer">
+      <a class="card-text text-danger ">${event.price}</a>
+      <a href="./Details.html" class="btn btn-danger">ver mas</a>
+      </div>
     </div>
   </div>
   </div>`
