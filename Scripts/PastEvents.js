efd175fda7afedd09212d4e57d51a2763cfa6c31
Exe <exegozategui@gmail.com>
2023-02-27 01:22:07 -0300

    se agrega js y dinamica a la pagina

---
+++ Scripts/PastEvents.js
@@ -0,0 +1,38 @@
+const containerCards = document.querySelector('#contenedor')
+
+let allCards = newCards(data.events)
+
+containerCards.innerHTML = allCards
+
+
+
+
+
+
+function newCards(arrayDatos){
+  let cards= ''
+
+  let currentDate= data.currentDate
+  for (const event of arrayDatos ) {
+  if (currentDate > event.date){
+
+
+    cards += `<div class="card-group">
+    <div class="card text-center " style= "width: 18rem;" >
+    <img src=${event.image} class="card-img-top" " alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${event.name}</h5>
+      <p class="card-text">${event.description}</p>
+      <a class="card-text">${event.price}</a>
+      <a href="./Details.html" class="btn btn-primary">ver mas</a>
+    </div>
+  </div>
+  </div>`
+}
+
+
+  }
+
+  return cards 
+
+}
---
+++ Scripts/main.js
@@ -0,0 +1,31 @@
+const containerCards = document.querySelector('#contenedor')
+
+let allCards = newCards(data.events)
+
+containerCards.innerHTML = allCards
+
+
+
+
+
+function newCards(arrayDatos){
+  let cards= ''
+  for (const event of arrayDatos ) {
+    
+    cards += `<div class="card-group">
+    <div class="card text-center " style= "width: 18rem;" >
+    <img src=${event.image} class="card-img-top" " alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${event.name}</h5>
+      <p class="card-text">${event.description}</p>
+      <a class="card-text">${event.price}</a>
+      <a href="./Details.html" class="btn btn-primary">ver mas</a>
+    </div>
+  </div>
+  </div>`
+
+  }
+
+  return cards 
+
+}
